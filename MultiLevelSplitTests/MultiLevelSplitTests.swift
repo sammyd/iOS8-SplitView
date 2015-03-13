cd49b9fdd59e8b045d2493f601b25df5a9fb993e
Sam Davies <sam@visualputty.co.uk>
2015-03-13 19:57:49 +0000

    Initial commit

---
+++ MultiLevelSplitTests/MultiLevelSplitTests.swift
@@ -0,0 +1,36 @@
+//
+//  MultiLevelSplitTests.swift
+//  MultiLevelSplitTests
+//
+//  Created by Sam Davies on 13/03/2015.
+//  Copyright (c) 2015 SamDavies. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class MultiLevelSplitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
